af9bdee9a558773101823be58e7ed518924488a6
Joel Hooks <joelhooks@gmail.com>
2026-01-17 16:13:33 -0800

    chore: add apps to vitest workspace

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,6 +1,10 @@
 import { defineWorkspace } from 'vitest/config'
 
 export default defineWorkspace([
+  'apps/web',
+  'apps/front',
+  'apps/slack',
+  'apps/docs',
   'packages/core',
   'packages/sdk',
   'packages/cli',
